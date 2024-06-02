62a69ed8d043a83e5bf7dc79c596002d8ea34727
Nalin Verma <88562136+nalinverma03@users.noreply.github.com>
2024-06-02 19:21:37 +0400

    Bug Fix - Corrected layering in tech stack item in the bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -119,19 +119,18 @@ export const BentoGridItem = ({
             "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
-          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
           <div className="font-sans font-extralight md:max-w-33 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
 
-          <div className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10 ${id !== 1 ? "text-center" : ""}`}>
+          <div className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10 ${id !== 1 ? "text-center" : ""} ${id === 1 ? "hidden md:block" : ""}`}>
             {title}
           </div>
 
-          {/* for the github 3d globe */}
+          {/*Globe*/}
           {id === 2 && <GridGlobe />}
 
-          {/* Tech stack list div */}
+          {/* Tech stack*/}
           {id === 1 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute right-3 lg:right-2">
               {/* tech stack lists */}
